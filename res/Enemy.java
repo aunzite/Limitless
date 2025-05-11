9385e64c351f2804118a900940ba90d0cab41a6b
Daoplex <s201096935@ddsbstudent.ca>
2025-05-10 22:38:41 -0400

    made Enemy

--- res/Enemy.java
+++ res/Enemy.java
@@ -1,5 +1,5 @@
 public class Enemy {
-    
+
     // Attributes
     private String name;
     private int health;
@@ -12,16 +12,41 @@ public class Enemy {
         damage = d;
     }
 
-    // Methods
+    // Method to attack
     public void attack() {
-        System.out.println(name + " attacks for " + damage + " damage!");
+        System.out.println(name + " attacks and deals " + damage + " damage!");
     }
 
+    // Method to take damage
     public void takeDamage(int dmg) {
         health = health - dmg;
+        if (health < 0) {
+            health = 0;
+        }
     }
 
+    // Method to check if enemy is alive
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    // Getter for health
     public int getHealth() {
         return health;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for damage
+    public int getDamage() {
+        return damage;
+    }
+
+    // Set new damage (example mutator)
+    public void setDamage(int newDamage) {
+        damage = newDamage;
     }
 }
